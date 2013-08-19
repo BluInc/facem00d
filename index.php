fafee3e025e493fdb6c2d3510cb90058c3768bce
Bowen Lu <bluinc@ubuntu.(none)>
2013-08-18 21:56:17 -0700

    launch

--- index.php
+++ index.php
@@ -260,8 +260,8 @@ function get_data($url) {
         <div align="center">
           
           <img id="frontpage" src="logo.png" alt="FaceMood"><br/>
-          <div style="margin: 0 auto;" class="fb-login-button" size="xlarge" data-scope="user_likes,user_photos,read_stream,publish_stream"></div>
-          <div class="fb-like" data-href="https://facem00d.herokuapp.com/" data-width="450" data-show-faces="true" data-send="true"></div>
+          <div style="margin: 0 auto;" class="fb-login-button" size="xlarge" data-scope="user_likes,user_photos,read_stream,publish_stream"></div><br/>
+          <div class="fb-like" data-href="https://facem00d.herokuapp.com/" data-width="450" data-colorscheme="dark" data-show-faces="true" data-send="true"></div>
         </div>
       </header>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
