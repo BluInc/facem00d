c8b99e0d9dc5ea89671a94ca4d24420f315f251f
Bowen Lu <bluinc@ubuntu.(none)>
2013-08-19 12:09:55 -0700

    now

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if ($user_id) {
     'method' => 'fql.query',
     'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
   ));*/
-}
+
 
 // Fetch the basic info of the app that they are using
 $app_info = $facebook->api('/'. AppInfo::appID());
