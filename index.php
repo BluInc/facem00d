6b5e0623ca2dcba9dc9967fbd41e12f0b1b11e1e
Bowen Lu <bluinc@ubuntu.(none)>
2013-08-20 20:42:24 -0700

    progress

--- index.php
+++ index.php
@@ -246,8 +246,30 @@ function get_data($url) {
           width: 400px;
           display:none;
       }
+      .popbox {
+    display: none;
+    position: absolute;
+    z-index: 99999;
+    width: 400px;
+    padding: 10px;
+    background: #EEEFEB;
+    color: #000000;
+    border: 1px solid #4D4F53;
+    margin: 0px;
+    -webkit-box-shadow: 0px 0px 5px 0px rgba(164, 164, 164, 1);
+    box-shadow: 0px 0px 5px 0px rgba(164, 164, 164, 1);
+}
+.popbox h2
+{
+    background-color: #4D4F53;
+    color:  #E3E5DD;
+    font-size: 14px;
+    display: block;
+    width: 100%;
+    margin: -10px 0px 8px -10px;
+    padding: 5px 10px;
+}
     </style>
-    <script src="bootstrap/js/bootstrap.js"></script>
   <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
     <script>
     function loading(){
@@ -265,6 +287,53 @@ function get_data($url) {
         $bar.width($bar.width()+40);
     }
 }, 1);}
+    $(function() {
+    var moveLeft = 0;
+    var moveDown = 0;
+    $('a.popper').hover(function(e) {
+   
+        var target = '#' + ($(this).attr('data-popbox'));
+         
+        $(target).show();
+        moveLeft = $(this).outerWidth();
+        moveDown = ($(target).outerHeight() / 2);
+    }, function() {
+        var target = '#' + ($(this).attr('data-popbox'));
+        $(target).hide();
+    });
+ 
+    $('a.popper').mousemove(function(e) {
+        var target = '#' + ($(this).attr('data-popbox'));
+         
+        leftD = e.pageX + parseInt(moveLeft);
+        maxRight = leftD + $(target).outerWidth();
+        windowLeft = $(window).width() - 40;
+        windowRight = 0;
+        maxLeft = e.pageX - (parseInt(moveLeft) + $(target).outerWidth() + 20);
+         
+        if(maxRight > windowLeft && maxLeft > windowRight)
+        {
+            leftD = maxLeft;
+        }
+     
+        topD = e.pageY - parseInt(moveDown);
+        maxBottom = parseInt(e.pageY + parseInt(moveDown) + 20);
+        windowBottom = parseInt(parseInt($(document).scrollTop()) + parseInt($(window).height()));
+        maxTop = topD;
+        windowTop = parseInt($(document).scrollTop());
+        if(maxBottom > windowBottom)
+        {
+            topD = windowBottom - $(target).outerHeight() - 20;
+        } else if(maxTop < windowTop){
+            topD = windowTop + 20;
+        }
+     
+        $(target).css('top', topD).css('left', leftD);
+     
+     
+    });
+ 
+});
     </script>
       <div style="background-color:#391256;">
       <div style="background-color:#391256;"> 
@@ -293,12 +362,25 @@ function get_data($url) {
         </div>
 
       </header>
+    <script src="bootstrap/js/bootstrap.js"></script>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      
+      <div id="pop1" class="popbox">
+    <h2>Success!</h2>
+    <p>This is an example popbox.</p>
+</div>
+<div id="pop2" class="popbox">
+    <h2>Danger!</h2>
+    <p>Don't let this popbox go off the screen!</p>
+</div>
+          <div id="footer">
+      <div class="container">
+        <p class="muted credit">FaceMood 2013 · This is a popbox test.  <a href="#" class="popper" data-popbox="pop1">Hover here</a> to see how it works.  You can also hover <a href="#" class="popper" data-popbox="pop2">here</a> to see a different example.</p>
+      </div>
+    </div>
       <?php } ?>
       
 
