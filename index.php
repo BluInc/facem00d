fde3363bf6941cd2e2b364326b32279ab8afa770
Bowen Lu <bluinc@ubuntu.(none)>
2013-08-29 10:16:35 -0700

    darned quotes

--- index.php
+++ index.php
@@ -566,7 +566,7 @@ function get_data($url) {
 						if(idx($status, 'link') == null){
 							$post_id = idx($status, 'id');
 
-							$returnHTML = '<div id="post' . he($post_id) . '" class="panel"><a href="https://www.facebook.com/' . he($id) . '" target="_top"><img src="https://graph.facebook.com/' . he($id) . '/picture?type=square" alt=" ' . he($name) . '">&nbsp;&nbsp;<strong>' .  he($name) . '</strong></a><br><br>' . he($message) . '<hr> <div class="row collapse"> <input id="' . he($post_id) . '" type="text" placeholder="Comment on their mood...">  <a href="#" onclick="submitComment(' . he($post_id) . ')" class="button prefix">Post</a></div> </div>';
+							$returnHTML = '<div id="post' . he($post_id) . '" class="panel"><a href="https://www.facebook.com/' . he($id) . '" target="_top"><img src="https://graph.facebook.com/' . he($id) . '/picture?type=square" alt=" ' . he($name) . '">&nbsp;&nbsp;<strong>' .  he($name) . '</strong></a><br><br>' . he($message) . '<hr> <div class="row collapse"> <input id="' . he($post_id) . '" type="text" placeholder="Comment on their mood...">  <a href="#" onclick="submitComment(\'' . he($post_id) . '\')" class="button prefix">Post</a></div> </div>';
 									  
 
 						}else{
